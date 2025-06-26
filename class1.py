b8984522b6d903eda6a810100f12d5c6ba68c85a
fahafarhan75 <fahafarhan75@gmail.com>
2025-06-26 20:43:35 +0500

    first commit

---
+++ class1.py
@@ -0,0 +1,12 @@
+print("hello, welcome to python journey")
+
+name=input("enter your name")
+print(f"my name is {name}")
+
+name=input("enter your name")
+print(f"hello {name}, welcome to python")
+ 
+number1=int(input("enter a  number"))
+number2=int(input("enter a number"))
+sum=number1 + number2
+print(f"the sum is:{sum}")
