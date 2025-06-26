6feadf59643bf025068c1a1aeae18122d7ffd367
fahafarhan75 <fahafarhan75@gmail.com>
2025-06-26 20:59:13 +0500

    third commit

---
+++ class3.py
@@ -0,0 +1,21 @@
+number=int(input("enter a number"))
+if number%2==0:
+  print(number,"is even")
+else:
+  print(number,"is odd")
+
+  number=int(input("enter your age"))
+if age>=18:
+  print("you are eligiable to vote")
+else:
+  print("you are not eligiable")   
+
+number=int(input("enter a number"))
+if number>0:
+  print(number,"is possitive")
+elif number==0:
+  print(number,"is zero")
+else:
+  print(number,"is negative")
+
+  
