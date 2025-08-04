04913dc58e9d6505e63a8d4656c126c15747d029
fahafarhan75 <fahafarhan75@gmail.com>
2025-08-04 17:20:34 +0500

    fifth commit

---
+++ class5.py
@@ -0,0 +1,10 @@
+for i in range(1,101):
+  sum=sum+i
+print(f"the sum of 100 numbers is {sum}")
+
+for i in range(0,11,2):
+  print(i)
+
+  fruits=['apple','banana','grapes','orange']
+for i in fruits:
+  print(i)
