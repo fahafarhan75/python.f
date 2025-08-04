63473288e8a75890297985178197caf94caaf645
fahafarhan75 <fahafarhan75@gmail.com>
2025-08-04 17:24:04 +0500

    eight commit

---
+++ class8.py
@@ -0,0 +1,29 @@
+student_scores={
+    "alice":85,
+    "bob":90,
+     "charlie":78
+}
+print(student_scores)
+ 
+ student_information={
+    "student1":
+    {
+     "name":"alice",
+     "major":"computer science",
+     "age":20
+     },
+  "student2":
+    {"name":"bob",
+    "major":"physics",
+    "age":22
+   }
+}
+print(student_information)
+
+students = [
+    {'name': 'Alice', 'score': 85},
+    {'name': 'Bob', 'score': 90}
+]
+print(students)
+ 
+ 
